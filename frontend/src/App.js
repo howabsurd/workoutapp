208b8e68433cc4b63643bf4601e88c9b83c6c36b
Krish Poptani <poptanikrish1@gmail.com>
2023-08-22 13:05:35 +0530

    setup Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.js";
+import Navbar from "./components/Navbar.js";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
