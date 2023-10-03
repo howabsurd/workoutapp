0eec341d6a6d4afbca38864c5153f27d0f11eab6
Krish Poptani <poptanikrish1@gmail.com>
2023-10-03 15:51:21 +0530

    backend + frontend

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,74 +1,103 @@
-const Workout = require("../models/WorkoutModel.js");
-const mongoose = require("mongoose");
-exports.getWorkouts = async (req, res) => {
-  try {
-    const workouts = await Workout.find({}).sort({ createdAt: -1 });
-    res.status(200).json(workouts);
-  } catch (error) {
-    console.error(`error: ${error}`);
-  }
-};
+const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
 
-exports.getWorkout = async (req, res) => {
-  const { id } = req.params;
+// get all workouts
+const getWorkouts = async (req, res) => {
+  const user_id = req.user._id
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such workout" });
-  }
+  const workouts = await Workout.find({user_id}).sort({createdAt: -1})
 
-  const workout = await Workout.findById(id);
+  res.status(200).json(workouts)
+}
 
-  if (!workout) {
-    return res.status(404).json({ error: "No such workout" });
-  }
-
-  res.status(200).json(workout);
-};
-
-exports.createWorkout = async (req, res) => {
-  const { title, load, reps } = req.body;
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ err: error.message });
-  }
-};
-
-exports.deleteWorkout = async (req, res) => {
-  const { id } = req.params;
+// get a single workout
+const getWorkout = async (req, res) => {
+  const { id } = req.params
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "No such workout" });
+    return res.status(404).json({error: 'No such workout'})
   }
 
-  const workout = await Workout.findOneAndDelete({ _id: id });
+  const workout = await Workout.findById(id)
 
   if (!workout) {
-    return res.status(400).json({ error: "No such workout" });
+    return res.status(404).json({error: 'No such workout'})
+  }
+  
+  res.status(200).json(workout)
+}
+
+
+// create new workout
+const createWorkout = async (req, res) => {
+  const {title, load, reps} = req.body
+
+  let emptyFields = []
+
+  if(!title) {
+    emptyFields.push('title')
+  }
+  if(!load) {
+    emptyFields.push('load')
+  }
+  if(!reps) {
+    emptyFields.push('reps')
+  }
+  if(emptyFields.length > 0) {
+    return res.status(400).json({ error: 'Please fill in all the fields', emptyFields })
   }
 
-  res.status(200).json(workout);
-};
+  // add doc to db
+  try {
+    const user_id = req.user._id
+    const workout = await Workout.create({title, load, reps, user_id})
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+// delete a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout'})
+  }
+
+  const workout = await Workout.findOneAndDelete({_id: id})
+
+  if (!workout) {
+    return res.status(400).json({error: 'No such workout'})
+  }
+
+  res.status(200).json(workout)
+}
 
 // update a workout
-exports.updateWorkout = async (req, res) => {
-  const { id } = req.params;
+const updateWorkout = async (req, res) => {
+  const { id } = req.params
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "No such workout" });
+    return res.status(404).json({error: 'No such workout'})
   }
 
-  const workout = await Workout.findOneAndUpdate(
-    { _id: id },
-    {
-      ...req.body,
-    }
-  );
+  const workout = await Workout.findOneAndUpdate({_id: id}, {
+    ...req.body
+  })
 
   if (!workout) {
-    return res.status(400).json({ error: "No such workout" });
+    return res.status(400).json({error: 'No such workout'})
   }
 
-  res.status(200).json(workout);
-};
+  res.status(200).json(workout)
+}
+
+
+module.exports = {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout
+}
